e608175b8af4b37cd84e7b29417a230a160411d8
Louis Arge <louislva@gmail.com>
2023-04-19 13:42:50 +0100

    login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,179 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+import { AtpAgentLoginOpts, BskyAgent } from "@atproto/api";
+import { ProfileView } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
 
-const inter = Inter({ subsets: ['latin'] })
+// TYPES
+type TimelineIdType = "chronological";
 
-export default function Home() {
+// HELPERS
+function paginateUntil(
+  fn: (cursor: string | undefined) => Promise<any[]>,
+  cursor: string | null = null
+) {}
+
+// TIMELINE SCREEN
+function TimelineScreen(props: {
+  identifier: string;
+  setIdentifier: (identifier: string | null) => void;
+  agent: BskyAgent;
+}) {
+  const { identifier, setIdentifier, agent } = props;
+  const [timelineId, setTimelineId] = useState<TimelineIdType>("chronological");
+  const [posts, setPosts] = useState<any[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      let follows: ProfileView[] = [];
+      let cursor;
+      for (let i = 0; i < 10; i++) {
+        const response = await agent.getFollows({
+          actor: identifier,
+          cursor,
+        });
+
+        if (response.success) {
+          follows = follows.concat(response.data.follows);
+          if (!response.data.cursor || response.data.follows.length === 0) {
+            break;
+          }
+          cursor = response.data.cursor;
+        } else {
+          // TODO: Handle error
+          break;
+        }
+        console.log(i, cursor, follows);
+      }
+    })();
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      {/* <TimelinePicker timelineId={timelineId} setTimelineId={setTimelineId} />
+      <Timeline posts={posts} /> */}
+    </div>
+  );
+}
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+// LOGIN SCREEN
+function LoginScreen(props: {
+  setIdentifier: (identifier: string | null) => void;
+  agent: BskyAgent;
+}) {
+  const { setIdentifier, agent } = props;
+  const login = (username: string, password: string) => {
+    setError(null);
+    agent
+      .login({
+        identifier: username,
+        password: password,
+      })
+      .then((response) => {
+        if (response.success) {
+          setIdentifier(response.data.did);
+        } else {
+          // Error
+          setIdentifier(null);
+          setError("Error");
+        }
+      })
+      .catch((err) => {
+        // Error
+        setIdentifier(null);
+        setError(err.message);
+      });
+  };
+
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<null | string>(null);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      {/* An offset equal to the security info (ish) */}
+      <div className="h-72" />
+      {/* The title */}
+      <h1 className="text-3xl font-bold mb-6">Login to Bluesky</h1>
+      {/* The login form */}
+      <form
+        className="flex flex-col"
+        onSubmit={(e) => {
+          e.preventDefault();
+          login(username, password);
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="border border-gray-300 p-2 rounded mb-4"
         />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border border-gray-300 p-2 rounded mb-4"
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+          Login
+        </button>
+      </form>
+      {error && <p className="text-red-500 mt-4">{error}</p>}
+      {/* Security policy section */}
+      <SecurityInfo />
+    </div>
+  );
+}
+function SecurityInfo() {
+  return (
+    <div className="mt-32 max-w-sm bg-white p-4 rounded-xl border rounded-xl">
+      <div className="flex flex-row pb-2 border-b mb-2">
+        <span className="material-icons mr-2 cursor-pointer">info</span>
+        <div>Is this secure?</div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
+      <b>Yes!</b> Bluesky unfortunately doesn't have an OAuth login system yet,
+      but we've taken the following measures to make sure your data is safe:
+      <ul>
+        <li>
+          We don't send your password to our own servers. Every request is made
+          directly from <i>your</i> browser to Bluesky's servers.
+        </li>
+        <li>
+          We don't store your password anywhere. Not on the backend, not on the
+          frontend, not in cookies, nowhere.
+        </li>
+        <li>
+          If you don't trust us, you can always check the source code of{" "}
+          <a
+            href="https://github.com/louislva/skyline"
+            className="text-blue-500"
+            target="_blank"
           >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            this website here.
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+export default function Main() {
+  const [identifier, setIdentifier] = useState<string | null>(null);
+  const agent = useRef<BskyAgent>(
+    new BskyAgent({
+      service: "https://bsky.social",
+    })
+  ).current;
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+  return identifier ? (
+    <TimelineScreen
+      identifier={identifier}
+      setIdentifier={setIdentifier}
+      agent={agent}
+    />
+  ) : (
+    <LoginScreen setIdentifier={setIdentifier} agent={agent} />
+  );
 }
