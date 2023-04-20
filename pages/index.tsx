09f9d9785854a7c4c5a7619925a87f6ea3f6ca30
Louis Arge <louislva@gmail.com>
2023-04-20 14:58:40 +0100

    followers / follows cache + getMutuals()

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,17 @@ import Link from "next/link";
 import { useEffect, useMemo, useRef, useState } from "react";
 const cosineSimilarity = require("compute-cosine-similarity");
 
+let followsCache: ProfileView[] | null = null;
+let followersCache: ProfileView[] | null = null;
+
 // HELPERS
 async function getFollows(
   agent: BskyAgent,
   identifier: string,
   maxPages: number = 10
-) {
+): Promise<ProfileView[]> {
+  if (followsCache) return followsCache;
+
   let follows: ProfileView[] = [];
   let cursor;
   for (let i = 0; i < maxPages; i++) {
@@ -36,7 +41,52 @@ async function getFollows(
       break;
     }
   }
+  followsCache = follows;
   return follows;
+}
+async function getFollowers(
+  agent: BskyAgent,
+  identifier: string,
+  maxPages: number = 10
+): Promise<ProfileView[]> {
+  if (followersCache) return followersCache;
+  let followers: ProfileView[] = [];
+  let cursor;
+  for (let i = 0; i < maxPages; i++) {
+    const response = await agent.getFollowers({
+      actor: identifier,
+      cursor,
+    });
+
+    if (response.success) {
+      followers = followers.concat(response.data.followers);
+      if (!response.data.cursor || response.data.followers.length === 0) {
+        break;
+      }
+      cursor = response.data.cursor;
+    } else {
+      // TODO: Handle error
+      break;
+    }
+  }
+  followersCache = followers;
+  return followers;
+}
+async function getMutuals(
+  agent: BskyAgent,
+  identifier: string
+): Promise<ProfileView[]> {
+  const followsPromise = getFollows(agent, identifier);
+  const followersPromise = getFollowers(agent, identifier);
+  const follows = await followsPromise;
+  const followers = await followersPromise;
+
+  const isDidAFollower = Object.fromEntries(
+    followers.map((f) => [f.did, true])
+  );
+
+  const mutuals = follows.filter((f) => isDidAFollower[f.did]);
+  return mutuals;
 }
 async function mergeConversationsContinual(
   agent: BskyAgent,
