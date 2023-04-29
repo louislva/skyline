2e2462349665791a9b79ce7df64102557602f2ea
Louis Arge <louislva@gmail.com>
2023-04-29 20:55:59 +0100

    Ensure sentry!

--- pages/api/embeddings.ts
+++ pages/api/embeddings.ts
@@ -6,20 +6,15 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req: any, res: any) {
-  try {
-    let text: string[] = req.body.text;
-    const response = await openai.createEmbedding({
-      model: "text-embedding-ada-002",
-      input: text,
-    });
+  let text: string[] = req.body.text;
+  const response = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input: text,
+  });
 
-    if (response.status === 200) {
-      res.status(200).json(response.data.data.map((item) => item.embedding));
-    } else {
-      res.status(500).json({ message: "Something went wrong" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
+  if (response.status === 200) {
+    res.status(200).json(response.data.data.map((item) => item.embedding));
+  } else {
+    throw new Error("OpenAI returned " + response.status);
   }
 }
--- pages/api/shared_custom_timeline.ts
+++ pages/api/shared_custom_timeline.ts
@@ -2,44 +2,39 @@ import { getDB } from "@/helpers/db";
 const { db } = getDB();
 
 export default async function handler(req: any, res: any) {
-  try {
-    if (req.method === "POST") {
-      const { config, created_by_handle } = req.body;
+  if (req.method === "POST") {
+    const { config, created_by_handle } = req.body;
 
-      if (typeof config !== "object") return res.status(400).end();
-      if (typeof created_by_handle !== "string") return res.status(400).end();
-      const key = `${Math.random().toString(36).substring(2, 7)}`;
+    if (typeof config !== "object") return res.status(400).end();
+    if (typeof created_by_handle !== "string") return res.status(400).end();
+    const key = `${Math.random().toString(36).substring(2, 7)}`;
 
-      await db.one(
-        `INSERT INTO shared_custom_timeline (key, config, created_by_handle) VALUES ($/key/, $/config/, $/created_by_handle/) RETURNING *`,
-        {
-          key,
-          config,
-          created_by_handle,
-        }
-      );
-      res.status(200).json({ key });
-    } else if (req.method === "GET") {
-      const key = req.query.key;
-      if (typeof key !== "string") return res.status(400).end();
-      const result = await db.one(
-        `SELECT config, created_by_handle FROM shared_custom_timeline WHERE key = $/key/`,
-        {
-          key,
-        }
-      );
-      await db.one(
-        `UPDATE shared_custom_timeline SET installs = installs + 1 WHERE key = $/key/ RETURNING *`,
-        {
-          key,
-        }
-      );
-      res.status(200).json(result);
-    } else {
-      return res.status(405).end();
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Something went wrong" });
+    await db.one(
+      `INSERT INTO shared_custom_timeline (key, config, created_by_handle) VALUES ($/key/, $/config/, $/created_by_handle/) RETURNING *`,
+      {
+        key,
+        config,
+        created_by_handle,
+      }
+    );
+    res.status(200).json({ key });
+  } else if (req.method === "GET") {
+    const key = req.query.key;
+    if (typeof key !== "string") return res.status(400).end();
+    const result = await db.one(
+      `SELECT config, created_by_handle FROM shared_custom_timeline WHERE key = $/key/`,
+      {
+        key,
+      }
+    );
+    await db.one(
+      `UPDATE shared_custom_timeline SET installs = installs + 1 WHERE key = $/key/ RETURNING *`,
+      {
+        key,
+      }
+    );
+    res.status(200).json(result);
+  } else {
+    return res.status(405).end();
   }
 }
