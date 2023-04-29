f6cdaf6232f0a0098996183f133df74ab74299a4
Louis Arge <louislva@gmail.com>
2023-04-29 02:13:06 +0100

    fix firefox!

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { useEffect } from "react";
+import Graphemer from "graphemer";
 
 export default function App({ Component, pageProps }: AppProps) {
+  // polyfill for firefox
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      let globalThis = window as any;
+
+      const splitter = new Graphemer();
+      globalThis.Intl = globalThis.Intl || {};
+
+      globalThis.Intl.Segmenter =
+        globalThis.Intl.Segmenter ||
+        class Segmenter {
+          constructor() {}
+          segment(s: string) {
+            return splitter.iterateGraphemes(s);
+          }
+        };
+    }
+  }, []);
   return (
     <>
       <Component {...pageProps} />
