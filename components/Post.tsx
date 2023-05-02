ed4153679000becdf7410a6c9b97c9cd695c9533
Louis Arge <louislva@gmail.com>
2023-05-02 20:11:00 +0100

    Two different renderers

--- components/Post.tsx
+++ components/Post.tsx
@@ -47,6 +47,18 @@ export default function Post(props: {
     post.postView.viewer?.like || null
   );
   const likeDiff = (isLiked ? 1 : 0) - (post.postView.viewer?.like ? 1 : 0);
+  const toggleLiked = async () => {
+    setIsLiked(!isLiked);
+    if (!isLiked) {
+      const { uri } = await agent.like(post.postView.uri, post.postView.cid);
+      setLikeUri(uri);
+    } else {
+      if (likeUri) {
+        agent.deleteLike(likeUri);
+      }
+      setLikeUri(null);
+    }
+  };
 
   const [isReposted, setIsReposted] = useState<boolean>(
     !!post.postView.viewer?.repost
@@ -56,6 +68,18 @@ export default function Post(props: {
   );
   const repostDiff =
     (isReposted ? 1 : 0) - (post.postView.viewer?.repost ? 1 : 0);
+  const toggleReposted = async () => {
+    setIsReposted(!isReposted);
+    if (!isReposted) {
+      const { uri } = await agent.repost(post.postView.uri, post.postView.cid);
+      setRepostUri(uri);
+    } else {
+      if (repostUri) {
+        agent.deleteRepost(repostUri);
+      }
+      setRepostUri(null);
+    }
+  };
 
   return (
     <>
@@ -106,35 +130,56 @@ export default function Post(props: {
               ))}
         </>
       )}
-      <ContentInline
-        agent={agent}
-        ancestorPosts={ancestorPosts}
-        post={post}
-        author={author}
-        record={record}
-        embed={embed}
-        images={images}
-        isLiked={isLiked}
-        setIsLiked={setIsLiked}
-        likeUri={likeUri}
-        setLikeUri={setLikeUri}
-        likeDiff={likeDiff}
-        isReposted={isReposted}
-        setIsReposted={setIsReposted}
-        repostUri={repostUri}
-        setRepostUri={setRepostUri}
-        repostDiff={repostDiff}
-        isSub={!!isSub}
-        hasChildren={!!hasChildren}
-        isLastPost={!!isLastPost}
-      />
+      {isStandAlone ? (
+        <ContentStandalone
+          agent={agent}
+          ancestorPosts={ancestorPosts}
+          post={post}
+          author={author}
+          record={record}
+          embed={embed}
+          images={images}
+          // Likes
+          isLiked={isLiked}
+          toggleLiked={toggleLiked}
+          likeCount={(post.postView.likeCount || 0) + likeDiff}
+          // Reposts
+          isReposted={isReposted}
+          toggleReposted={toggleReposted}
+          repostCount={(post.postView.repostCount || 0) + repostDiff}
+          // Other
+          isSub={!!isSub}
+          hasChildren={!!hasChildren}
+          isLastPost={!!isLastPost}
+        />
+      ) : (
+        <ContentInline
+          agent={agent}
+          ancestorPosts={ancestorPosts}
+          post={post}
+          author={author}
+          record={record}
+          embed={embed}
+          images={images}
+          // Likes
+          isLiked={isLiked}
+          toggleLiked={toggleLiked}
+          likeCount={(post.postView.likeCount || 0) + likeDiff}
+          // Reposts
+          isReposted={isReposted}
+          toggleReposted={toggleReposted}
+          repostCount={(post.postView.repostCount || 0) + repostDiff}
+          // Other
+          isSub={!!isSub}
+          hasChildren={!!hasChildren}
+          isLastPost={!!isLastPost}
+        />
+      )}
     </>
   );
 }
 
-function ContentStandalone() {}
-
-function ContentInline(props: {
+function ContentStandalone(props: {
   agent: BskyAgent;
 
   ancestorPosts: SkylinePostType[];
@@ -145,16 +190,12 @@ function ContentInline(props: {
   images: ImageType[];
 
   isLiked: boolean;
-  setIsLiked: (liked: boolean) => void;
-  likeUri: string | null;
-  setLikeUri: (liked: string | null) => void;
-  likeDiff: number;
+  toggleLiked: () => void;
+  likeCount: number;
 
   isReposted: boolean;
-  setIsReposted: (liked: boolean) => void;
-  repostUri: string | null;
-  setRepostUri: (liked: string | null) => void;
-  repostDiff: number;
+  toggleReposted: () => void;
+  repostCount: number;
 
   isSub: boolean; // If we're not at the root level
   hasChildren: boolean;
@@ -171,16 +212,254 @@ function ContentInline(props: {
     images,
 
     isLiked,
-    setIsLiked,
-    likeUri,
-    setLikeUri,
-    likeDiff,
+    toggleLiked,
+    likeCount,
 
     isReposted,
-    setIsReposted,
-    repostUri,
-    setRepostUri,
-    repostDiff,
+    toggleReposted,
+    repostCount,
+
+    isSub,
+    hasChildren,
+    isLastPost,
+  } = props;
+
+  const repostBy = post.repostBy;
+
+  return (
+    <Link
+      href={`/profile/${author.handle}/post/${
+        post.postView.uri?.split("/")?.slice(-1)?.[0]
+      }`}
+    >
+      <div
+        className={
+          "p-4 bg-red-500 overflow-hidden " +
+          (hasChildren || isLastPost
+            ? "border-none "
+            : "border-b " + BORDER_300)
+        }
+      >
+        {/* Reply / repost row */}
+        {(record.reply || repostBy) && (
+          <div
+            className={
+              "flex flex-row items-center text-sm pt-2 pb-2 -mt-4 text-slate-700 dark:text-slate-300 " +
+              (record.reply && (ancestorPosts.length || isSub)
+                ? "border-t border-dashed " + BORDER_300
+                : "")
+            }
+          >
+            {record.reply && (
+              <>
+                <div className="material-icons mr-1">reply</div>
+                <div>Replied</div>
+              </>
+            )}
+            {record.reply && repostBy && (
+              <div className="ml-2 mr-1 border-l h-3 w-0 border-slate-600 dark:border-slate-400"></div>
+            )}
+            {repostBy && (
+              <>
+                <div className="material-icons mr-1">repeat</div>
+                <div>Reposted by {repostBy.displayName}</div>
+              </>
+            )}
+          </div>
+        )}
+
+        {/* Profile row */}
+        <div className="flex flex-row ">
+          <Link
+            href={`/profile/${author.handle}`}
+            className="flex flex-row hover:underline"
+          >
+            {/* Pfp */}
+            {author.avatar && (
+              <div className="w-12 h-12 mr-3 rounded-full overflow-hidden">
+                <img src={author.avatar} alt={author.name + "'s avatar"} />
+              </div>
+            )}
+            {/* Name / handle */}
+            <div className="flex flex-col">
+              <div className="font-semibold">{author.displayName}</div>
+              <div className="text-slate-500 dark:text-slate-400">
+                {author.handle ===
+                "deepfates.com.deepfates.com.deepfates.com.deepfates.com.deepfates.com"
+                  ? "i'm an asshole 💩"
+                  : "@" + author.handle}
+              </div>
+            </div>
+          </Link>
+          {/* timestamp */}
+          <div className="flex-grow text-right text-slate-500 dark:text-slate-400">
+            {moment(post.postView.indexedAt).fromNow()}
+          </div>
+        </div>
+        {/* Content row */}
+        <div className="mt-2">
+          {record.text.split("\n").map((line, index) => (
+            <Fragment key={line + "$" + index}>
+              {index !== 0 && <br />}
+              {line}
+            </Fragment>
+          ))}
+        </div>
+        {/* Images */}
+        {!!images?.length && (
+          <div className="mt-2 flex flex-row h-72 gap-4">
+            {images?.slice(0, 3).map((image) => (
+              <div
+                className="flex-1 rounded-md overflow-hidden"
+                key={image.thumb}
+              >
+                <img
+                  src={image.thumb}
+                  alt={image.alt}
+                  className="h-full w-full object-cover"
+                />
+              </div>
+            ))}
+          </div>
+        )}
+        {/* Quote tweets */}
+        {embed?.record?.value?.text && (
+          <div
+            className={"mt-2 border rounded-md p-2 py-2 text-sm " + BORDER_300}
+          >
+            <div className="flex flex-row items-center h-4 text-slate-700 dark:text-slate-300 bg-green-4000 mb-1">
+              <img
+                src={embed.record.author.avatar}
+                className="w-4 h-4 rounded-full mr-1"
+              />
+              <span className="font-semibold mr-1 leading-none">
+                {embed.record.author.displayName}
+              </span>
+              <span className="text-slate-500 dark:text-slate-400 leading-none">
+                {" "}
+                @{embed.record.author.handle}
+              </span>
+            </div>
+            <div className="bg-blue-4000">
+              {embed?.record?.value?.text?.split("\n").map((line, index) => (
+                <Fragment key={line + "$" + index}>
+                  {index !== 0 && <br />}
+                  {line}
+                </Fragment>
+              ))}
+            </div>
+          </div>
+        )}
+        {/* Likes, RTs, etc. row */}
+        <div className="flex flex-row items-center text-base mt-3 text-slate-700 dark:text-slate-300 leading-none">
+          <div className="material-icons mr-1">chat_bubble_outline</div>
+          <div className="mr-4">{post.postView.replyCount}</div>
+          <div
+            className="rounded-full hover:bg-green-500/20 p-2 -m-2 flex justify-center items-center -mr-1"
+            onClick={async (e) => {
+              e.preventDefault();
+              toggleReposted();
+            }}
+          >
+            <div
+              className={
+                "material-icons " +
+                (isReposted
+                  ? "text-green-500"
+                  : "text-slate-700 dark:text-slate-300")
+              }
+              style={{
+                paddingRight: 0.66 / 16 + "rem",
+              }}
+            >
+              repeat
+            </div>
+          </div>
+          <div className="mr-4">{repostCount}</div>
+          <div
+            className="rounded-full hover:bg-red-500/20 p-2 -m-2 flex justify-center items-center -mr-1"
+            onClick={async (e) => {
+              e.preventDefault();
+              toggleLiked();
+            }}
+          >
+            <div
+              className={
+                "material-icons " +
+                (isLiked
+                  ? "text-red-500"
+                  : "text-slate-700 dark:text-slate-300")
+              }
+              style={{
+                paddingRight: 0.66 / 16 + "rem",
+              }}
+            >
+              {isLiked ? "favorite" : "favorite_border"}
+            </div>
+          </div>
+          <div className="mr-4">{likeCount}</div>
+          {post.score && (
+            <>
+              {/* cog icon / settings icon bec it's a machine */}
+              <div className="material-icons ml-auto mr-1 text-slate-400">
+                settings
+              </div>
+              <div className="text-slate-400">
+                {/* {(post.score * 100).toFixed(2)} */}
+                {(
+                  Math.pow(Math.abs(post.score), 0.3) *
+                  Math.sign(post.score) *
+                  100
+                ).toFixed(0)}
+                % match
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+function ContentInline(props: {
+  agent: BskyAgent;
+
+  ancestorPosts: SkylinePostType[];
+  post: SkylinePostType;
+  author: PostView["author"];
+  record: RecordType;
+  embed: EmbedType | undefined;
+  images: ImageType[];
+
+  isLiked: boolean;
+  toggleLiked: () => void;
+  likeCount: number;
+
+  isReposted: boolean;
+  toggleReposted: () => void;
+  repostCount: number;
+
+  isSub: boolean; // If we're not at the root level
+  hasChildren: boolean;
+  isLastPost: boolean;
+}) {
+  const {
+    agent,
+
+    ancestorPosts,
+    post,
+    author,
+    record,
+    embed,
+    images,
+
+    isLiked,
+    toggleLiked,
+    likeCount,
+
+    isReposted,
+    toggleReposted,
+    repostCount,
 
     isSub,
     hasChildren,
@@ -321,19 +600,7 @@ function ContentInline(props: {
             className="rounded-full hover:bg-green-500/20 p-2 -m-2 flex justify-center items-center -mr-1"
             onClick={async (e) => {
               e.preventDefault();
-              setIsReposted(!isReposted);
-              if (!isReposted) {
-                const { uri } = await agent.repost(
-                  post.postView.uri,
-                  post.postView.cid
-                );
-                setRepostUri(uri);
-              } else {
-                if (repostUri) {
-                  agent.deleteRepost(repostUri);
-                }
-                setRepostUri(null);
-              }
+              toggleReposted();
             }}
           >
             <div
@@ -350,26 +617,12 @@ function ContentInline(props: {
               repeat
             </div>
           </div>
-          <div className="mr-4">
-            {(post.postView.repostCount || 0) + repostDiff}
-          </div>
+          <div className="mr-4">{repostCount}</div>
           <div
             className="rounded-full hover:bg-red-500/20 p-2 -m-2 flex justify-center items-center -mr-1"
             onClick={async (e) => {
               e.preventDefault();
-              setIsLiked(!isLiked);
-              if (!isLiked) {
-                const { uri } = await agent.like(
-                  post.postView.uri,
-                  post.postView.cid
-                );
-                setLikeUri(uri);
-              } else {
-                if (likeUri) {
-                  agent.deleteLike(likeUri);
-                }
-                setLikeUri(null);
-              }
+              toggleLiked();
             }}
           >
             <div
@@ -386,9 +639,7 @@ function ContentInline(props: {
               {isLiked ? "favorite" : "favorite_border"}
             </div>
           </div>
-          <div className="mr-4">
-            {(post.postView.likeCount || 0) + likeDiff}
-          </div>
+          <div className="mr-4">{likeCount}</div>
           {post.score && (
             <>
               {/* cog icon / settings icon bec it's a machine */}
