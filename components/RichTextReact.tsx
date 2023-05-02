562982c42efac993e67e35befa8a7e95cab1a9cc
Louis Arge <louislva@gmail.com>
2023-05-02 19:21:50 +0100

    Componentize

---
+++ components/RichTextReact.tsx
@@ -0,0 +1,87 @@
+import { LINK } from "@/helpers/styling";
+import { BskyAgent, RichText } from "@atproto/api";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function RichTextReact(props: {
+  agent: BskyAgent;
+  text: string;
+}) {
+  const { agent, text } = props;
+  const [segments, setSegments] = useState<
+    {
+      type: "text" | "link" | "mention";
+      text: string;
+      value?: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      const rt = new RichText({
+        text,
+      });
+      await rt.detectFacets(agent); // automatically detects mentions and links
+
+      let newSegments: typeof segments = [];
+
+      // @ts-expect-error
+      for (const segment of rt.segments()) {
+        if (segment.isLink()) {
+          newSegments.push({
+            type: "link",
+            text: segment.text,
+            value: segment.link?.uri,
+          });
+        } else if (segment.isMention()) {
+          newSegments.push({
+            type: "mention",
+            text: segment.text,
+            value: segment.mention.did,
+          });
+        } else {
+          newSegments.push({
+            type: "text",
+            text: segment.text,
+          });
+        }
+      }
+
+      setSegments(newSegments);
+    })();
+  }, [text]);
+
+  return (
+    <>
+      {segments.map((segment) => {
+        return (
+          <>
+            {segment.text.split("\n").map((line, index) => {
+              return (
+                <>
+                  {index !== 0 && <br />}
+                  {segment.type === "link" ? (
+                    <a
+                      href={segment.value}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={LINK}
+                    >
+                      {line}
+                    </a>
+                  ) : segment.type === "mention" ? (
+                    <Link href={`/profile/${segment.value}`} className={LINK}>
+                      {line}
+                    </Link>
+                  ) : (
+                    line
+                  )}
+                </>
+              );
+            })}
+          </>
+        );
+      })}
+    </>
+  );
+}
--- pages/profile/[handle]/index.tsx
+++ pages/profile/[handle]/index.tsx
@@ -1,96 +1,16 @@
 import { LoadingPlaceholder } from "@/components/LoadingSpinner";
 import Post from "@/components/Post";
+import RichTextReact from "@/components/RichTextReact";
 import {
   feedViewPostToSkylinePost,
   mergeConversationsInstant,
 } from "@/helpers/bsky";
 import { SkylinePostType } from "@/helpers/contentTypes";
-import { BORDER_200, BORDER_300, LINK } from "@/helpers/styling";
-import { BskyAgent, RichText } from "@atproto/api";
+import { BORDER_200, BORDER_300 } from "@/helpers/styling";
+import { BskyAgent } from "@atproto/api";
 import { ProfileViewDetailed } from "@atproto/api/dist/client/types/app/bsky/actor/defs";
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-function RichTextReact(props: { agent: BskyAgent; text: string }) {
-  const { agent, text } = props;
-  const [segments, setSegments] = useState<
-    {
-      type: "text" | "link" | "mention";
-      text: string;
-      value?: string;
-    }[]
-  >([]);
-
-  useEffect(() => {
-    (async () => {
-      const rt = new RichText({
-        text,
-      });
-      await rt.detectFacets(agent); // automatically detects mentions and links
-
-      let newSegments: typeof segments = [];
-
-      // @ts-expect-error
-      for (const segment of rt.segments()) {
-        if (segment.isLink()) {
-          newSegments.push({
-            type: "link",
-            text: segment.text,
-            value: segment.link?.uri,
-          });
-        } else if (segment.isMention()) {
-          newSegments.push({
-            type: "mention",
-            text: segment.text,
-            value: segment.mention.did,
-          });
-        } else {
-          newSegments.push({
-            type: "text",
-            text: segment.text,
-          });
-        }
-      }
-
-      setSegments(newSegments);
-    })();
-  }, [text]);
-
-  return (
-    <>
-      {segments.map((segment) => {
-        return (
-          <>
-            {segment.text.split("\n").map((line, index) => {
-              return (
-                <>
-                  {index !== 0 && <br />}
-                  {segment.type === "link" ? (
-                    <a
-                      href={segment.value}
-                      target="_blank"
-                      rel="noreferrer"
-                      className={LINK}
-                    >
-                      {line}
-                    </a>
-                  ) : segment.type === "mention" ? (
-                    <Link href={`/profile/${segment.value}`} className={LINK}>
-                      {line}
-                    </Link>
-                  ) : (
-                    line
-                  )}
-                </>
-              );
-            })}
-          </>
-        );
-      })}
-    </>
-  );
-}
 
 // TIMELINE SCREEN
 export type ProfileScreenProps = {
