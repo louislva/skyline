98af9008c98ca521f70d6d5026b95633f93a69d1
Louis Arge <louislva@gmail.com>
2023-05-07 19:45:32 +0100

    Sync, part one: Posting

--- db.sql
+++ db.sql
@@ -21,3 +21,28 @@ alter table shared_custom_timeline
 
 alter table shared_custom_timeline
     add config_new jsonb;
+
+create table "user"
+(
+    id         serial
+        primary key
+        unique,
+    did        text                                         not null
+        unique,
+    created_on timestamp with time zone default now()       not null,
+    handle     text                                         not null,
+    config     jsonb                    default '{}'::jsonb not null
+);
+
+create table user_visit
+(
+    id            serial
+        primary key
+        unique,
+    "user"        int               not null
+        constraint user_visit_user_id_fk
+            references "user"
+            on update cascade on delete cascade,
+    created_on    timestamp with time zone default now() not null,
+    posted_config jsonb             not null
+);
